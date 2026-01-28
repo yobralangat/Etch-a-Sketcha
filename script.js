c650b9282b97651ee761051542bd17d1e16219c2
kuryot <briancherujimz1@gmail.com>
2026-01-28 17:25:58 +0300

    Create lightweight documentFragment Appending divs directly into the container slows the page. Creating a fragment in memory then appending the div items to the fragment then appending the fragment to the container solves this problem.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createGrid () {
     const container = document.getElementById('container');
-    const userInput = Number(prompt('Enter a number to make one side of the grid'));
+    const userInput = Number(prompt('Enter a number of squares per side'));
     input = userInput * userInput;
     //The user input should be a number between 1 and 100.
     if (isNaN(userInput) || userInput <= 0 || userInput > 100) {
@@ -9,18 +9,21 @@ function createGrid () {
     }
     //Define the width of a grid item.
     const itemWidth = 100 / userInput;
+    const fragment = document.createDocumentFragment();
+    const itemHeight = container.offsetHeight / userInput;
     // Create the grid
     for(let i = 0; i < input; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         gridItem.style.flex = `0 0 ${itemWidth}%`;
-        gridItem.style.height = `${container.offsetHeight / userInput}px`;
+        gridItem.style.height = `${itemHeight}px`;
         // Add event listener that changes the background color of the cells on mouse hover.
         gridItem.addEventListener('mouseover', () => {
             gridItem.style.backgroundColor = 'yellow';
         });
-        container.appendChild(gridItem);
+    fragment.appendChild(gridItem);
     }
+container.appendChild(fragment);
 }
 
 createGrid();
